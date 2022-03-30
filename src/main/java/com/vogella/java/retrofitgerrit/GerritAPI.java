300c9aaf8b33bd808bdafb09b3f4a0f5b3f7c9c9
claudiaggg <claudia.garcia.gil@estudiantat.upc.edu>
2022-03-30 22:21:09 +0200

    esto funciona

---
+++ src/main/java/com/vogella/java/retrofitgerrit/GerritAPI.java
@@ -0,0 +1,12 @@
+package com.vogella.java.retrofitgerrit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface GerritAPI {
+    @GET("changes/")
+    Call<List<Track>> loadChanges(@Query("q") String status);
+}
---
+++ src/main/java/com/vogella/java/retrofitgerrit/Track.java
@@ -0,0 +1,14 @@
+package com.vogella.java.retrofitgerrit;
+
+public class Track {
+    String subject;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+}
